0f74e0fa16b79979f9eb5c638b7ac3ad55bf042a
cbrgm <chris@cbrgm.de>
2018-08-08 15:49:00 +0200

    Removed i386 compile step as there is no pre compiled i386 hugo version available at the moment

--- .drone.sh
+++ .drone.sh
@@ -7,4 +7,3 @@ set -x
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0         go build -ldflags "-X main.build=${DRONE_BUILD_NUMBER}" -a -tags netgo -o release/linux/amd64/drone-hugo github.com/drone-plugins/drone-hugo/cmd/drone-hugo
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0         go build -ldflags "-X main.build=${DRONE_BUILD_NUMBER}" -a -tags netgo -o release/linux/arm64/drone-hugo github.com/drone-plugins/drone-hugo/cmd/drone-hugo
 GOOS=linux GOARCH=arm   CGO_ENABLED=0 GOARM=7 go build -ldflags "-X main.build=${DRONE_BUILD_NUMBER}" -a -tags netgo -o release/linux/arm/drone-hugo   github.com/drone-plugins/drone-hugo/cmd/drone-hugo
-GOOS=linux GOARCH=386   CGO_ENABLED=0         go build -ldflags "-X main.build=${DRONE_BUILD_NUMBER}" -a -tags netgo -o release/linux/i386/drone-hugo  github.com/drone-plugins/drone-hugo/cmd/drone-hugo
