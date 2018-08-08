28c712dc9818c92611dd663f1f8789a9ee2399fc
cbrgm <chris@cbrgm.de>
2018-08-08 21:49:22 +0200

    Fix integration test

--- download/download_test.go
+++ download/download_test.go
@@ -6,13 +6,13 @@ import (
 
 func TestDownloadURL(t *testing.T) {
 	want := "https://github.com/gohugoio/hugo/releases/download/v1.0/hugo_1.0_Linux-64bit.tar.gz"
-	if got := downloadURL("1.0", "64bit"); got != want {
+	if got := downloadURL("1.0"); got != want {
 		t.Errorf("Download url is not correct, got: %s, want: %s", got, want)
 	}
 }
 
 func TestGet(t *testing.T) {
-	if binPath , err := Get("0.42","64bit"); err != nil {
+	if binPath , err := Get("0.42"); err != nil {
 		t.Errorf("Failed to download archive: %s", err)
 		if binPath == "" {
 			t.Errorf("binPath was nil")
