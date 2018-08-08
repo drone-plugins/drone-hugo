ffff344c85cc7089732ab712ca0cc2469e14b4e4
cbrgm <chris@cbrgm.de>
2018-08-08 16:04:06 +0200

    Change version/build naming to drone-plugins default

--- cmd/drone-hugo/main.go
+++ cmd/drone-hugo/main.go
@@ -9,15 +9,17 @@ import (
 	"github.com/urfave/cli"
 )
 
-var build = "0" // build number set at compile-time
-var version = "2.0"
+var (
+	version = "0.0.0"
+	build   = "0"
+)
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "hugo plugin"
 	app.Usage = "hugo plugin"
 	app.Action = run
-	app.Version = fmt.Sprintf("%s build %s", version, build)
+	app.Version = fmt.Sprintf("%s+%s", version, build)
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:   "buildDrafts",
